c5454f14cfb7debe0546db065262be0a40e4287e
Erik Olofsson <erik@olofsson.info>
2016-05-31 16:58:18 +0200

    [Malterlib/Test] Add concurrent logging support to test app (#2)

--- Tools/Tester/Source/Main.cpp
+++ Tools/Tester/Source/Main.cpp
@@ -5,6 +5,13 @@
 #include <Mib/Core/Application>
 #include <Mib/Test/Test>
 
+#define DEnableLogging 0
+
+#if DEnableLogging
+#	include <Mib/Log/Destinations>
+#	include <Mib/Concurrency/ConcurrencyManager>
+#endif
+
 class CTestApplication : public NMib::CApplication
 {
 public:
@@ -16,8 +23,56 @@ public:
 	{
 		NMib::NContainer::TCVector<NMib::NStr::CStr> CommandLine;
 		NMib::NSys::fg_Process_GetCommandLineArgs(CommandLine);
-		
+
 		NMib::fg_GetSys()->f_RemoveAllLoggers();
+#if DMibSysLogSeverities && DEnableLogging
+		using namespace NMib;
+		using namespace NMib::NLog;
+		auto LoggerActor = NMib::NConcurrency::fg_ConstructActor<NMib::NConcurrency::CSeparateThreadActor>(fg_Construct("Log"));
+		NMib::fg_GetSys()->f_GetLogger().f_PushGlobalDestination
+			(
+				[LoggerActor]
+				(
+					mint _ThreadID
+					, NTime::CTime const& _Time
+					, ESeverity _Sev
+					, CLogStr const& _Message
+					, DMibListLinkDS_List(CSysLogCatScope, m_Link) const &_Categories
+					, DMibListLinkDS_List(CSysLogOpScope, m_Link) const &_Operations
+					, CLogLocationTag const& _Loc
+				)
+				{
+					auto Category = (_Categories.f_IsEmpty() ? NStr::CStrNonTracked() : NStr::fg_Format<NStr::CStrNonTracked>("<{}>", _Categories.f_GetFirst()->m_pCategory));
+					fg_Dispatch
+						(
+							LoggerActor
+							, [=]
+							{
+								NTime::CTimeConvert::CDateTime DateTime;
+								NTime::CTimeConvert(_Time.f_ToLocal()).f_ExtractDateTime(DateTime);
+								DMibConErrOut
+									(
+										"{}-{sj2,sf0}-{sj2,sf0} {sj2,sf0}:{sj2,sf0}:{sj2,sf0}.{fr1,fe3} {sj32} {sj10} {}{\n}"
+										, DateTime.m_Year
+										<< DateTime.m_Month
+										<< DateTime.m_DayOfMonth
+										<< DateTime.m_Hour
+										<< DateTime.m_Minute
+										<< DateTime.m_Second
+										<< DateTime.m_Fraction
+										<< Category 
+										<< NStr::fg_Format<NStr::CStrNonTracked>("[{}]", fg_GetSeverityName(_Sev))
+										<< _Message
+									)
+								;
+							}
+						)
+						> NMib::NConcurrency::fg_DiscardResult()
+					;
+				}
+			)
+		;
+#endif
 		
 		for (auto iArg = CommandLine.f_GetIterator(); iArg; ++iArg)
 		{
@@ -47,7 +102,14 @@ public:
 		}
 		
 		auto MemoryManagerCheckout = NMib::fg_GetSys()->f_MemoryManager_Checkout();
-		return NMib::NTest::fg_RunTests();
+		auto Ret = NMib::NTest::fg_RunTests();
+
+#if DMibSysLogSeverities && DEnableLogging
+		NMib::fg_GetSys()->f_RemoveAllLoggers();
+		LoggerActor->f_BlockDestroy();
+		NMib::fg_GetSys()->f_AddStdErrLogger();
+#endif
+		return Ret;
 	}
 
 };
