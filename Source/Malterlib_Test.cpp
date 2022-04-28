ccf76ff36b1ab596d5bce1c3c6814420338a5b0c
Erik Olofsson <erik@olofsson.info>
2022-04-28 23:26:55 +0200

    [Malterlib/Test] Refactor intrusive ref-count

--- Source/Malterlib_Test.cpp
+++ Source/Malterlib_Test.cpp
@@ -294,7 +294,7 @@ namespace NMib::NTest
 						NStorage::TCSharedPointer<NConcurrency::CDefaultRunLoop> pRunLoop = fg_Construct();
 						auto CleanupRunLoop = g_OnScopeExit / [&]
 							{
-								while (pRunLoop->f_RefCountGet() > 0)
+								while (pRunLoop->m_RefCount.f_Get() > 0)
 									pRunLoop->f_WaitOnceTimeout(0.1);
 							}
 						;
